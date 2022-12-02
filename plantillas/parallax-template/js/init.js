0342ee9126103a9c7828c8943f34400dd75dd196
ur1e1 <113937441+ur1e1@users.noreply.github.com>
2022-12-02 19:58:51 +0100

    s

--- plantillas/parallax-template/js/init.js
+++ plantillas/parallax-template/js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $('.dropdown-trigger').dropdown();
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
