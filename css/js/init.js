de292058e5371575309d822a5b4372695e7382ff
ur1e1 <uriel.raipar@educa.jcyl.es>
2022-12-07 15:13:15 +0100

    s

--- css/js/init.js
+++ css/js/init.js
@@ -16,6 +16,18 @@
       indicators: true
     });
     
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, options);
+    });
+  
+    // Or with jQuery
+  
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+          
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
